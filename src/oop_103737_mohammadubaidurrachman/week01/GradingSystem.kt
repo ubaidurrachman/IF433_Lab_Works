597e8bb5cf0452044c87eaf0f07cc886e41f950e
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-08 23:43:47 +0700

    week01: add calculateStatus function

--- src/oop_103737_mohammadubaidurrachman/week01/GradingSystem.kt
+++ src/oop_103737_mohammadubaidurrachman/week01/GradingSystem.kt
@@ -2,13 +2,10 @@ package oop_103737_mohammadubaidurrachman.week01
 
 fun main() {
     val score = 85
-
-    val grade = when {
-        score >= 85 -> "A"
-        score >= 75 -> "B"
-        else -> "C"
-    }
-
-    println("Grade = $grade")
+    println(calculateStatus(score))
 }
+
+fun calculateStatus(score: Int) =
+    if (score >= 75) "Pass" else "Fail"
+
 
