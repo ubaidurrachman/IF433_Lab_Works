ac2b7ba2e2056e92af4a737d1e40909ff4a09ef4
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-08 23:43:03 +0700

    week01: refactor to use val and string templates

--- src/oop_103737_mohammadubaidurrachman/week01/GradingSystem.kt
+++ src/oop_103737_mohammadubaidurrachman/week01/GradingSystem.kt
@@ -1,11 +1,6 @@
 package oop_103737_mohammadubaidurrachman.week01
 
 fun main() {
-    var score: Int = 85
-
-    if (score >= 75) {
-        println("Pass")
-    } else {
-        println("Fail")
-    }
+    val score = 85
+    println("Score = $score")
 }
