000334314c5edda2b126dc360b2db6dc49396a8a
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-08 23:42:21 +0700

    week01: initial setup and procedural code

---
+++ src/oop_103737_mohammadubaidurrachman/week01/GradingSystem.kt
@@ -0,0 +1,11 @@
+package oop_103737_mohammadubaidurrachman.week01
+
+fun main() {
+    var score: Int = 85
+
+    if (score >= 75) {
+        println("Pass")
+    } else {
+        println("Fail")
+    }
+}
