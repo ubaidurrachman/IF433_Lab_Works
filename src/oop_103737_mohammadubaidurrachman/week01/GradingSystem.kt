dab7187d6f9b70e3d1a8100b5573157c42a12851
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-08 23:49:03 +0700

    week01: implement null safety check

--- src/oop_103737_mohammadubaidurrachman/week01/GradingSystem.kt
+++ src/oop_103737_mohammadubaidurrachman/week01/GradingSystem.kt
@@ -1,9 +1,12 @@
 package oop_103737_mohammadubaidurrachman.week01
 
-fun main() {
-    val score = 85
-    println(calculateStatus(score))
-}
-
 fun calculateStatus(score: Int) =
     if (score >= 75) "Pass" else "Fail"
+
+fun main() {
+    val score: Int? = null
+    val safeScore = score ?: 0
+
+    println(calculateStatus(safeScore))
+}
+
