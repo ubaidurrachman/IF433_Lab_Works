ac9fe4042012ea95e56cf7e58ba923bf05ff3a05
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-08 23:56:59 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_103737_mohammadubaidurrachman/week01/GameStore.kt
+++ src/oop_103737_mohammadubaidurrachman/week01/GameStore.kt
@@ -4,3 +4,7 @@ fun main() {
     val gameTitle = "Cyber Adventure"
     val price = 600000
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 500000) price * 80 / 100
+    else price * 90 / 100
