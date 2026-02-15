e31d6d1c651af9324379ba0b0af4626d0e9ba701
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-15 20:45:15 +0700

    week02: add warning validation in init block

--- src/oop_103737_mohammadubaidurrachman/week02/Student.kt
+++ src/oop_103737_mohammadubaidurrachman/week02/Student.kt
@@ -1,7 +1,19 @@
 package oop_103737_mohammadubaidurrachman.week02
 
+
 class Student(
     val nim: String,
     var name: String,
     var major: String
-)
+) {
+
+    init {
+        if (nim.length != 10) {
+            println("WARNING: NIM harus 10 digit!")
+        }
+
+        if (name.isBlank()) {
+            println("WARNING: Nama tidak boleh kosong!")
+        }
+    }
+}
