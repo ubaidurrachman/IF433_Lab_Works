7c3d9970ab7f63bb8f9294e16b73e3128f370476
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-15 21:03:51 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_103737_mohammadubaidurrachman/week02/Hero.kt
@@ -0,0 +1,23 @@
+package oop_103737_mohammadubaidurrachman.week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+
+    fun attack(targetName: String) {
+        println("$name menyerang $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+}
