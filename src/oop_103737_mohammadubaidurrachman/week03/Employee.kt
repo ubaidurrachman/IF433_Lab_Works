ec9d224561e78a546ee0df1129c1cfc62da955cd
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-20 15:35:06 +0700

    week03: simulate stack overflow error in setter

--- src/oop_103737_mohammadubaidurrachman/week03/Employee.kt
+++ src/oop_103737_mohammadubaidurrachman/week03/Employee.kt
@@ -2,5 +2,11 @@ package oop_103737_mohammadubaidurrachman.week03
 
 class Employee(
     val name: String,
-    var salary: Int
-)
+    salary: Int
+) {
+
+    var salary: Int = salary
+        set(value) {
+            this.salary = value //  sengaja salah (infinite recursion)
+        }
+}
---
+++ src/oop_103737_mohammadubaidurrachman/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_103737_mohammadubaidurrachman.week03
+
+fun main() {
+    val e = Employee("Budi", 1000)
+    e.salary = 5000
+    println(e.salary)
+}
