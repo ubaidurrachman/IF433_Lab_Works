bfb0ec8c03e77d45b35a806cea8e17a8e9974a50
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-20 15:39:55 +0700

    "week03: test all encapsulation logic in main"

--- src/oop_103737_mohammadubaidurrachman/week03/Employee.kt
+++ src/oop_103737_mohammadubaidurrachman/week03/Employee.kt
@@ -14,10 +14,8 @@ class Employee(
             }
         }
 
-    // 🔒 private property
     private var performanceRating: Int = 0
 
-    // public method untuk update aman
     fun updatePerformanceRating(newRating: Int) {
         if (newRating in 1..5) {
             performanceRating = newRating
@@ -25,4 +23,8 @@ class Employee(
             println("Rating harus antara 1 sampai 5")
         }
     }
+
+    // COMPUTED PROPERTY
+    val bonus: Int
+        get() = salary / 10
 }
--- src/oop_103737_mohammadubaidurrachman/week03/Main.kt
+++ src/oop_103737_mohammadubaidurrachman/week03/Main.kt
@@ -1,7 +1,15 @@
 package oop_103737_mohammadubaidurrachman.week03
 
 fun main() {
+
+    println("=== Employee Test ===")
     val e = Employee("Budi", 1000)
+
     e.salary = 5000
-    println(e.salary)
+    e.salary = -100
+
+    e.updatePerformanceRating(4)
+
+    println("Salary: ${e.salary}")
+    println("Bonus: ${e.bonus}")
 }
