c111085a8353d4014d8d2829d0d92daeacd8237b
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-24 15:21:09 +0700

    week04: override honk method in Car"

--- src/oop_103737_mohammadubaidurrachman/week04/Car.kt
+++ src/oop_103737_mohammadubaidurrachman/week04/Car.kt
@@ -1,8 +1,11 @@
 package oop_103737_mohammadubaidurrachman.week04
 
 open class Car(brand: String, val numberOfDoors: Int) : Vehicle(brand) {
-
+    override fun honk(){
+        println("TIN TIN! Mobil $brand lewat!")
     fun openTrunk() {
         println("Bagasi mobil $brand dengan $numberOfDoors pintu dibuka.")
+
+        }
     }
 }
