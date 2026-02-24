c51d2f8f0be8ec96d8a69505e60ef9e652ffdc09
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-24 16:47:44 +0700

    "week04: (task 1) create ElectricCar with final override"

--- src/oop_103737_mohammadubaidurrachman/week04/Main.kt
+++ src/oop_103737_mohammadubaidurrachman/week04/Main.kt
@@ -6,6 +6,10 @@ fun main() {
     generatedVehicle.honk()
     generatedVehicle.accelerate()
 
+    fun main() {
+        val tesla = ElectricCar("Tesla", 4, 90)
+        tesla.accelerate()
+    }
     println("\n--- Testing Car ---")
     val myCar = Car(brand = "Toyota", 4)
     myCar.openTrunk()
