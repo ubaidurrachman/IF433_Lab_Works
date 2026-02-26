0cd3e85db6a3854e446b196c3504de8102cadee0
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-26 12:06:59 +0700

    week04: (task 2) create Developer class

---
+++ src/oop_103737_mohammadubaidurrachman/week04/Developer.kt
@@ -0,0 +1,13 @@
+package oop_103737_mohammadubaidurrachman.week04
+
+class Developer(
+    name: String,
+    baseSalary: Int,
+    val programmingLanguage: String
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+    // calculateBonus() menggunakan bawaan Employee
+}
