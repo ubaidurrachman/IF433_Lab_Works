0eefb5a410b1b5ea850e08badf8254e6f653fb9c
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-02-26 12:05:33 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_103737_mohammadubaidurrachman/week04/Manager.kt
@@ -0,0 +1,15 @@
+package oop_103737_mohammadubaidurrachman.week04
+
+class Manager(
+    name: String,
+    baseSalary: Int
+) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500_000
+    }
+}
