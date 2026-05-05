98fced078062f110acfbd4dcad930ba98d822528
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-05 15:35:49 +0700

    week11: create string extension functions

---
+++ src/oop_103737_mohammadubaidurrachman/week11/StringExtensions.kt
@@ -0,0 +1,14 @@
+package oop_103737_mohammadubaidurrachman.week11
+
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
+
+fun String?.isNullOrEmptyCustom(): Boolean {
+    return this == null || this.isEmpty()
+}
