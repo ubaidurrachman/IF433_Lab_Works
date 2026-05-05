dbe115a16466751a6554549a9182566d3342f267
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-05 15:40:09 +0700

    week11: test extension functions in main

---
+++ src/oop_103737_mohammadubaidurrachman/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_103737_mohammadubaidurrachman.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
