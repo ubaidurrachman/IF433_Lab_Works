d74761965491fad294f2f3ad4fdc0608c2ca8473
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-19 15:48:38 +0700

    git add . git commit -m "week13: create Student model"

---
+++ src/oop_103737_mohammadubaidurrachman/week13/StudentSerialization.kt
@@ -0,0 +1,10 @@
+package oop_103737_mohammadubaidurrachman.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val gpa: Double
+)
