34821ff265586a81f909311fe772a917ef8656f5
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-19 15:29:50 +0700

    git add . git commit -m "week13: implement basic writeText function"

---
+++ src/oop_103737_mohammadubaidurrachman/week13/BasicFileIO.kt
@@ -0,0 +1,14 @@
+package oop_103737_mohammadubaidurrachman.week13
+
+import java.io.File
+
+fun main() {
+
+    println("=== TEST WRITE TEXT ===")
+
+    val file = File("notes.txt")
+
+    file.writeText("Line 1: Inisialisasi sistem.\n")
+
+    println("File berhasil dibuat dan ditulis.")
+}
