be59ff7741d6ec2ce2fed29a32f4c898026d8cdc
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-19 16:00:20 +0700

    git add . git commit -m "week13: (task) calculate total PnL from loaded trades"

--- src/oop_103737_mohammadubaidurrachman/week13/CryptoTradeLogger.kt
+++ src/oop_103737_mohammadubaidurrachman/week13/CryptoTradeLogger.kt
@@ -75,4 +75,9 @@ fun main() {
     File("crypto_trades.csv")
         .appendText("CORRUPT_ID,DOGEUSDT,Hold,XX,YY\n")
 }
+val loadedData = loadTrades("crypto_trades.csv")
 
+val totalPnl = loadedData.sumOf {
+
+    it.pnl
+}
