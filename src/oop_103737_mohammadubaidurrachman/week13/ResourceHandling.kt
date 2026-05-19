2a20a5e2611ff4f686cd493bf12873bc664c4abe
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-19 15:46:58 +0700

    git add . git commit -m "week13: implement printWriter with safe use block"

--- src/oop_103737_mohammadubaidurrachman/week13/ResourceHandling.kt
+++ src/oop_103737_mohammadubaidurrachman/week13/ResourceHandling.kt
@@ -16,4 +16,18 @@ fun main() {
     writer.close()
 
     println("Proses penulisan unsafe selesai.")
+
+    println("\n=== TEST SAFE RESOURCE HANDLING ===")
+
+    val safeFile = File("safe_logs.txt")
+
+    safeFile.printWriter().use { out ->
+
+        for (i in 1..100) {
+
+            out.println("Safe Log entry #$i: System status OK.")
+        }
+    }
+
+    println("100 baris log berhasil di-generate dengan sangat aman.")
 }
