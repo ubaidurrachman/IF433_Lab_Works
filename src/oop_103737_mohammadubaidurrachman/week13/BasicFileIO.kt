076c7a8d86c7339f26ebe626973dd2eb6c131cfe
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-19 15:41:43 +0700

    git add . git commit -m "week13: implement appendText operation"

---
+++ src/oop_103737_mohammadubaidurrachman/week13/BasicFileIO.kt
@@ -0,0 +1,18 @@
+package oop_103737_mohammadubaidurrachman.week13
+
+import java.io.File
+
+fun main() {
+
+    println("=== TEST WRITE TEXT ===")
+
+    val file = File("notes.txt")
+
+    file.writeText("Line 1: Inisialisasi sistem.\n")
+
+    println("File berhasil dibuat dan ditulis.")
+
+    file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
+
+    println("Teks berhasil di-append.")
+}
