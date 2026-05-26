8082427fade093db3c70f2bea2b8ff0f04f85fb2
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-26 14:18:49 +0700

    git add . git commit -m "week14: implement Shape independently to fix LSP"

--- src/oop_103737_mohammadubaidurrachman/week14/LspRefactored.kt
+++ src/oop_103737_mohammadubaidurrachman/week14/LspRefactored.kt
@@ -3,3 +3,17 @@ package oop_103737_mohammadubaidurrachman.week14
 interface Shape {
     fun area(): Int
 }
+class SafeRectangle(
+    var width: Int,
+    var height: Int
+) : Shape {
+
+    override fun area() = width * height
+}
+
+class SafeSquare(
+    var side: Int
+) : Shape {
+
+    override fun area() = side * side
+}
