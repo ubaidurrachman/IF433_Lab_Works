572372f12afed2ab3d156ae0e5eca82a9ebc15e2
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-26 14:11:32 +0700

    git add . git commit -m "week14: implement basic writeText function"

---
+++ src/oop_103737_mohammadubaidurrachman/week14/SrpViolation.kt
@@ -0,0 +1,25 @@
+package oop_103737_mohammadubaidurrachman.week14
+
+data class User(
+    val name: String,
+    val email: String,
+    val age: Int
+)
+
+class UserManager {
+
+    // Business logic
+    fun validateUser(user: User): Boolean {
+        return user.email.contains("@") && user.age >= 18
+    }
+
+    // Database access
+    fun saveUserToDatabase(user: User) {
+        println("INSERT INTO users VALUES ('${user.name}', '${user.email}')")
+    }
+
+    // Email notification
+    fun sendWelcomeEmail(user: User) {
+        println("Sending email to: ${user.email}")
+    }
+}
