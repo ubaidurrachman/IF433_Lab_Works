2affdbe3b32c875673488a5bfa7069b9cd9848b9
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-26 14:19:41 +0700

    git add . git commit -m "week14: create fat MultifunctionDevice (ISP violation)"

---
+++ src/oop_103737_mohammadubaidurrachman/week14/IspViolation.kt
@@ -0,0 +1,26 @@
+package oop_103737_mohammadubaidurrachman.week14
+
+
+interface MultifunctionDevice {
+
+    fun print(doc: String)
+
+    fun scan(doc: String): String
+
+    fun fax(doc: String)
+}
+
+class SimplePrinter : MultifunctionDevice {
+
+    override fun print(doc: String) {
+        println("Printing $doc")
+    }
+
+    override fun scan(doc: String): String {
+        throw UnsupportedOperationException("No scanner")
+    }
+
+    override fun fax(doc: String) {
+        throw UnsupportedOperationException("No fax")
+    }
+}
