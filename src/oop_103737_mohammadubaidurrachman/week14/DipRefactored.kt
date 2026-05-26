7828d4e74d1e2c49f925a49a8c85d442c00d104d
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-26 14:22:37 +0700

    git add . git commit -m "week14: create Database abstraction and implementations"

---
+++ src/oop_103737_mohammadubaidurrachman/week14/DipRefactored.kt
@@ -0,0 +1,13 @@
+package oop_103737_mohammadubaidurrachman.week14
+
+interface Database {
+
+    fun query(sql: String): List<String>
+}
+
+class PostgresDatabase : Database {
+
+    override fun query(sql: String): List<String> {
+        return listOf("pg_data1", "pg_data2")
+    }
+}
