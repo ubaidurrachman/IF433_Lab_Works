194fa34145630a664075eb62955584f4bb74a87f
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-26 14:14:07 +0700

    git add . git commit -m "week14: create bad DiscountCalculator (OCP violation)"

---
+++ src/oop_103737_mohammadubaidurrachman/week14/OcpViolation.kt
@@ -0,0 +1,14 @@
+package oop_103737_mohammadubaidurrachman.week14
+
+class DiscountCalculator {
+
+    fun calculate(price: Double, type: String): Double {
+
+        return when(type) {
+            "student" -> price * 0.80
+            "member" -> price * 0.85
+            "employee" -> price * 0.70
+            else -> price
+        }
+    }
+}
