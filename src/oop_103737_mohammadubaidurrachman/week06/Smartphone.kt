50797484d6ac7db7880b72daad4d6bf883ebe03b
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-29 02:57:34 +0700

    git commit -m "week06: fix diamond problem using explicit super delegation"

--- src/oop_103737_mohammadubaidurrachman/week06/Smartphone.kt
+++ src/oop_103737_mohammadubaidurrachman/week06/Smartphone.kt
@@ -2,4 +2,12 @@ package oop_103737_mohammadubaidurrachman.week06
 
 class Smartphone : Camera, Phone {
 
+    override fun turnOn() {
+
+        super<Camera>.turnOn()
+
+        super<Phone>.turnOn()
+
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }
