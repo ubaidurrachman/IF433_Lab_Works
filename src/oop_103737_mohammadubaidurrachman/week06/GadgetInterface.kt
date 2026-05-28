4e100af7cb84a531b41d1aeb846111a05b2ec857
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-29 02:54:22 +0700

    git commit -m "week06: create Camera and Phone interfaces with default methods"

---
+++ src/oop_103737_mohammadubaidurrachman/week06/GadgetInterface.kt
@@ -0,0 +1,17 @@
+package oop_103737_mohammadubaidurrachman.week06
+
+interface Camera {
+
+    fun turnOn() {
+
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+
+    fun turnOn() {
+
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
