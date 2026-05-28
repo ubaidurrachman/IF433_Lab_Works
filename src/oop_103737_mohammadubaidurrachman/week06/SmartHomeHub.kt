93c790712478f52f2c4f7c75fb5cde58458e78fa
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-29 03:09:12 +0700

    git commit -m "week06: (task) create SmartHomeHub logic"

---
+++ src/oop_103737_mohammadubaidurrachman/week06/SmartHomeHub.kt
@@ -0,0 +1,38 @@
+package oop_103737_mohammadubaidurrachman.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+
+        devices.add(device)
+    }
+
+    fun turnOffAllSwitches() {
+
+        for (device in devices) {
+
+            if (device is Switchable) {
+
+                device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode() {
+
+        for (device in devices) {
+
+            if (device is Recordable) {
+
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
