6d1774dabb624098c2b47eb9c4c45e957e6e7a04
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-29 02:51:51 +0700

    git commit -m "week06: create Smartwatch with multiple implementations"

---
+++ src/oop_103737_mohammadubaidurrachman/week06/Smartwatch.kt
@@ -0,0 +1,22 @@
+package oop_103737_mohammadubaidurrachman.week06
+
+class Smartwatch :
+    Watch(),
+    BluetoothConnectable,
+    Rechargeable {
+
+    override fun showTime() {
+
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
