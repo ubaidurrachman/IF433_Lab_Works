7232c7c184ecdf934dba72362c010c8a633ce083
ubaidurrachman <164150031+ubaidurrachman@users.noreply.github.com>
2026-05-29 02:18:27 +0700

    git commit -m "week05: implement polymorphic collection and smart casting"

---
+++ src/oop_103737_mohammadubaidurrachman/week05/Main.kt
@@ -0,0 +1,45 @@
+package oop_103737_mohammadubaidurrachman.week05
+
+fun main() {
+
+    val dosen1 = Dosen(
+        nama = "Pak Alex",
+        nidn = "0123456"
+    )
+
+    val admin1 = Admin(
+        nama = "Bu Siti"
+    )
+
+    // Polymorphic Collection
+    val daftarPegawai: List<Pegawai> =
+        listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+
+    for (pegawai in daftarPegawai) {
+
+        // Runtime Polymorphism
+        pegawai.bekerja()
+
+        // Smart Casting
+        when (pegawai) {
+
+            is Dosen -> {
+
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+
+                pegawai.mengajar()
+            }
+
+            is Admin -> {
+
+                println("=> Terdeteksi sebagai Admin")
+
+                pegawai.doAdminWork()
+            }
+        }
+
+        println("----------------------")
+    }
+}
